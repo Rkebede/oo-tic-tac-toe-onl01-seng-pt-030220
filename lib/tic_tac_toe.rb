64be1830ee61d7cc1d13e55f4e91b6a791c4cecc
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-16 14:38:53 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,101 @@
+class TicTacToe
+ 
+ WIN_COMBINATIONS = [
+   [ 0, 1 ,2 ],
+   [ 3, 4, 5 ], 
+   [ 6, 7, 8 ],
+   [ 0, 4, 8 ],
+   [ 2, 4, 6 ], 
+   [ 0, 3, 6 ],
+   [ 1, 4, 7 ],
+   [ 2, 5, 8 ]
+   ] 
+ 
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} " 
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} " 
+  end
+  
+  def input_to_index(input)
+    user_move = input.to_i - 1
+  end 
+  
+  def move(board_index, token = "X")
+    @board[board_index] = token
+  end
+  
+  def position_taken?(index)
+    if @board[index] == " " || @board[index] == "" || @board[index] == nil
+      false
+    else 
+      true
+    end
+  end 
+  
+  def valid_move?(index)
+    if self.position_taken?(index) || !index.between?(0,8)
+      false
+    else 
+      true
+    end 
+  end
+  
+  def turn_count
+    @board.count do |index|
+      index != " " && index != nil
+    end 
+  end 
+
+  def current_player
+    if turn_count.even? 
+      return "X"
+    else
+      return "O"
+    end
+  end 
+  
+  def turn
+    puts "Make a move"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    token = current_player
+    if valid_move?(index)
+      move(index, token)
+      display_board
+    else
+      turn
+    end
+  end 
+  
+  def won?
+    x_arr = []
+    o_arr = []
+    draw = !@board.include?(" ")
+ 
+    if draw
+      return false
+    end
+    
+    @board.each_with_index do |character, index|
+      if character == "X"
+        x_arr << index
+      elsif character == "O"
+        o_arr << index
+      end
+    
+    WIN_COMBINATIONS.each do |combination|
+      combination.include?(x_arr)
+        return x_arr
+      combination.include?(o_arr)
+        return o_arr
+      end
+    end
+  end
+end
